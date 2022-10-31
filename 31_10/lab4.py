2276a5f38b4827a00e03cc423a2bea9fd483f75a
Redrouthu <lr22aak@herts.ac.uk>
2022-10-31 12:59:15 +0000

    changes

--- 31_10/lab4.py
+++ 31_10/lab4.py
@@ -9,6 +9,7 @@ import clean_up as cl
 
 with open('C:/Users/lr22aak/OneDrive - University of Hertfordshire/ads1/31_10/big_data.txt', 'r') as text:
     all_words = []
+    all_letters = []
     counter = 0
     for line in text:
         words = line.split()
@@ -16,6 +17,9 @@ with open('C:/Users/lr22aak/OneDrive - University of Hertfordshire/ads1/31_10/bi
     # print(counter)
         for word in words:
             word = cl.clean(word)
+            letters = list(word)
+            for l in letters:
+                all_letters.append(l)
             all_words.append(word)
             
 # print("all words: ", all_words)
@@ -30,4 +34,10 @@ count_words = df_words['words'].value_counts()
 
 # help(pd.DataFrame())
 
-print(df_words['words'][100])
+# print(df_words['words'][100])
+
+df_letters = pd.DataFrame(data = all_words, columns = ('letters', ))
+
+count_words = df_letters['letters'].value_counts()
+
+
